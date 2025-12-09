cff3645f39f706a51ec517768a38fd5b6afca2d6
shafqat mehmood <130746320+shokimalik786@users.noreply.github.com>
2025-12-09 22:36:32 +0500

    Add files via upload

---
+++ Conns.java
@@ -0,0 +1,22 @@
+
+import java.sql.*;
+public class Conns {
+    Statement stmt;
+    Connection c;
+    public Conns(){  
+        try{  
+        Class.forName("com.mysql.cj.jdbc.Driver");  
+        c = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/tourism",
+            "root",
+            "123456789"
+        );
+                //here tms is database name, root is username and password  
+        stmt=c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+    
+}
+}
